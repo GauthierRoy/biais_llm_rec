fcb8406d3e2be00e997bdb2efc8821f567b0ec4a
AlexandreAndr <alexandreandre02018@gmail.com>
2025-04-10 18:22:07 -0400

    :sparkles:

---
+++ pipeline.sh
@@ -0,0 +1,39 @@
+echo "Running pipeline.sh"
+
+# Manage virtual environment
+if [ -n "$VIRTUAL_ENV" ]; then
+    deactivate
+fi
+
+if [ -d "venv" ]; then
+    echo "Virtual environment already exists, skipping creation."
+    source venv/bin/activate
+else
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install .
+fi
+
+
+# Manage the dataset creation
+mkdir -p data/datasets
+dataset_list=()
+for file in dataset_creation/*.py; do
+    dataset_list+=("$(basename "${file%.py}")")
+done
+
+for dataset in "${dataset_list[@]}"; do
+    if [ -f "data/datasets/$dataset.json" ]; then
+        echo "$dataset dataset already exists, skipping creation."
+    else
+        echo "Creating dataset $dataset..."
+        python "dataset_creation/${dataset}.py"  
+        echo "$dataset dataset created."
+    fi
+done
+
+# launch the inference 
+python inference.py
+
+# compute the metrics
+python result.py
